5dbc1a600d7a3d02cf4382c2f8ced29d6a082dfc
Anais <anafloreaq@gmail.com>
2022-05-14 22:07:41 +0300

    first commit spring boot

---
+++ src/main/java/uni/java/springboot/ProjectOopApplication.java
@@ -0,0 +1,13 @@
+package uni.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOopApplication.class, args);
+	}
+
+}
